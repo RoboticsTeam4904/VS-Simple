77c4fb132f2217cb4a3403d0e6864676046cee64
Daniel Bernstein <danbern@nuevaschool.org>
2020-01-19 23:28:15 -0800

    2020 changes

--- src/main/java/org/usfirst/frc4904/robot/Robot.java
+++ src/main/java/org/usfirst/frc4904/robot/Robot.java
@@ -4,7 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project. */
 /*----------------------------------------------------------------------------*/
-package frc4904.robot;
+package org.usfirst.frc4904.robot;
 
 
 import edu.wpi.first.wpilibj.Joystick;

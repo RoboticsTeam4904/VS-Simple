b2c457b679cfc7245b2a04fe1fa87b13f9caca3a
Exr0n <mail@exr0n.com>
2023-02-01 02:54:30 -0800

    mvp for sim; sus standard

--- src/main/java/org/usfirst/frc4904/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc4904/robot/RobotMap.java
@@ -1,13 +1,35 @@
 package org.usfirst.frc4904.robot;
 
+import org.usfirst.frc4904.standard.custom.CustomPIDSourceType;
+import org.usfirst.frc4904.standard.custom.controllers.CustomCommandJoystick;
+import org.usfirst.frc4904.standard.custom.motioncontrollers.CANTalonFX;
+import org.usfirst.frc4904.standard.custom.sensors.CANTalonEncoder;
+import org.usfirst.frc4904.standard.custom.sensors.CustomCANCoder;
+import org.usfirst.frc4904.standard.custom.sensors.EncoderPair;
+import org.usfirst.frc4904.standard.custom.sensors.NavX;
+import org.usfirst.frc4904.standard.custom.sensors.PDP;
+import org.usfirst.frc4904.standard.subsystems.chassis.TankDrive;
+import org.usfirst.frc4904.standard.subsystems.motor.Motor;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotMap {
     public static class Port {
         public static class HumanInput {
+            public static final int JOYSTICK = 0;
+            public static final int XBOX_CONTROLLER = 1;
         }
 
         public static class CANMotor {
+
+            public static final int LEFT_DRIVE_A = 0;
+            public static final int RIGHT_DRIVE_A = 0;
+            public static final int RIGHT_DRIVE_B = 0;
+            public static final int LEFT_DRIVE_B = 0;
         }
 
         public static class PWM {
@@ -34,6 +56,12 @@ public class RobotMap {
             public static final double DISTANCE_SIDE_SIDE = -1;
             public static final double INCHES_PER_TICK = Metrics.Chassis.CIRCUMFERENCE_INCHES
                     / Metrics.Chassis.TICKS_PER_REVOLUTION;
+        }
+
+        public static class Encoders {
+            public static class TalonEncoders {
+                public static final double REVOLUTIONS_PER_TICK = 2048;
+            }
         }
     }
 
@@ -65,6 +93,14 @@ public class RobotMap {
     }
 
     public static class Component {
+        public static PDP pdp;
+        public static NavX navx;
+        public static Motor rightWheelA;
+        public static Motor rightWheelB;
+        public static Motor leftWheelA;
+        public static Motor leftWheelB;
+        public static CANTalonEncoder leftWheelTalonEncoder;
+        public static CANTalonEncoder rightWheelTalonEncoder;
     }
 
     public static class Input {
@@ -72,13 +108,55 @@ public class RobotMap {
 
     public static class HumanInput {
         public static class Driver {
+            public static CommandXboxController xbox;
         }
 
         public static class Operator {
+            public static CustomCommandJoystick joystick;
         }
     }
 
+    /**
+     * 
+     */
     public RobotMap() {
+        HumanInput.Driver.xbox = new CommandXboxController(Port.HumanInput.XBOX_CONTROLLER);
+        HumanInput.Operator.joystick = new CustomCommandJoystick(Port.HumanInput.JOYSTICK);
 
+        /* General */
+        Component.pdp = new PDP();
+        Component.navx = new NavX(SerialPort.Port.kMXP);
+        /* Drive Train */
+
+        // Wheels
+        CANTalonFX rightWheelATalon = new CANTalonFX(Port.CANMotor.RIGHT_DRIVE_A);
+        CANTalonFX rightWheelBTalon = new CANTalonFX(Port.CANMotor.RIGHT_DRIVE_B);
+        CANTalonFX leftWheelATalon  = new CANTalonFX(Port.CANMotor.LEFT_DRIVE_A);
+        CANTalonFX leftWheelBTalon  = new CANTalonFX(Port.CANMotor.LEFT_DRIVE_B);
+        Component.rightWheelA = new Motor("rightWheelA", false, rightWheelATalon);
+        Component.rightWheelB = new Motor("rightWheelB", false, rightWheelBTalon);
+        Component.leftWheelA  = new Motor("leftWheelA",  true,  leftWheelATalon);
+        Component.leftWheelB  = new Motor("leftWheelB",  true,  leftWheelBTalon);
+
+        // Wheel Encoders
+        Component.leftWheelTalonEncoder = new CANTalonEncoder("Leftwheel", leftWheelATalon, true,
+                Metrics.Encoders.TalonEncoders.REVOLUTIONS_PER_TICK, CustomPIDSourceType.kDisplacement,
+                FeedbackDevice.IntegratedSensor);
+        Component.rightWheelTalonEncoder = new CANTalonEncoder("rightWheel", rightWheelATalon, true,
+                Metrics.Encoders.TalonEncoders.REVOLUTIONS_PER_TICK, CustomPIDSourceType.kDisplacement,
+                FeedbackDevice.IntegratedSensor);
+
+        // Component.leftWheelCANCoder = new CustomCANCoder(Port.CAN.LEFT_WHEEL_ENCODER,
+        //         Metrics.Chassis.CAN_CODER_METERS_PER_TICK);
+        // Component.rightWheelCANCoder = new CustomCANCoder(Port.CAN.RIGHT_WHEEL_ENCODER,
+        //         Metrics.Chassis.CAN_CODER_METERS_PER_TICK);
+
+        // Component.chassisTalonEncoders = new EncoderPair(Component.leftWheelTalonEncoder, Component.rightWheelCANCoder);
+        // Component.chassisCANCoders = new EncoderPair(Component.leftWheelCANCoder, Component.rightWheelCANCoder);
+
+        // // General Chassis
+        // Component.chassis = new TankDrive("Blinky-Chassis", Component.leftWheelA, Component.leftWheelB,
+        //         Component.rightWheelA, Component.rightWheelB);
+        // Component.chassis.setDefaultCommand(new ChassisMove(Component.chassis, new NathanGain()));
     }
 }
